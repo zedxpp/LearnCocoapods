a042d1dc2e11191a69ffadcd4519ad2318b9fb79
zedxpp <zedxpp@gmail.com>
2018-07-19 11:26:40 +0800

    init

---
+++ LearnCocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnCocoapods
+//
+//  Created by pengpeng on 2018/7/19.
+//  Copyright © 2018年 zedxpp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
